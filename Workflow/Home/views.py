b40da2812c1365bb1527e20bb6069b4a4c5e7528
AditeeKore <aditeembk@gmail.com>
2021-08-23 15:06:38 +0530

    working on views.py

--- Workflow/Home/views.py
+++ Workflow/Home/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render , HttpResponse
 
 
+def index(request):
+    # return HttpResponse("this is homepage")
+    return render(request, 'index.html')
+
+def login(request):
+    return HttpResponse("this is login page")
+
+def faq(request):
+    return render(request, 'faq.html')
