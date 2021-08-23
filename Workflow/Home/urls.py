99ff5f9ac1071a41223faa9278fcb6e27b4e9137
dharmil143 <dharmilkaria143@gmail.com>
2021-08-23 14:37:43 +0530

    views

--- Workflow/Home/urls.py
+++ Workflow/Home/urls.py
@@ -1,4 +1,3 @@
-
 """Workflow URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
@@ -15,9 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from Home import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('',include('Home.urls'))
+    path("", views.index, name='Home')
 ]
